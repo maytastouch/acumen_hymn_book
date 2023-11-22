8e54a510028f527c099131b4ddf270c9e66f4e51
maytastouch <tanyamutelembi@gmail.com>
2023-11-22 16:12:49 +0200

    Almost

--- lib/theme_bloc/theme_bloc.dart
+++ lib/theme_bloc/theme_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../core/app_themes.dart';
 
@@ -10,10 +11,25 @@ part 'theme_state.dart';
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
   ThemeBloc()
       : super(ThemeState(themeData: appThemeData[AppTheme.LightTheme]!)) {
-    on<ThemeEvent>((event, emit) {
+    _loadTheme();
+
+    on<ThemeEvent>((event, emit) async {
       if (event is ThemeChanged) {
+        await _saveTheme(event.theme);
         emit.call(ThemeState(themeData: appThemeData[event.theme]!));
       }
     });
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeIndex = prefs.getInt('theme') ?? AppTheme.LightTheme.index;
+    final theme = AppTheme.values[themeIndex];
+    add(ThemeChanged(theme: theme));
+  }
+
+  Future<void> _saveTheme(AppTheme theme) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('theme', theme.index);
   }
 }
